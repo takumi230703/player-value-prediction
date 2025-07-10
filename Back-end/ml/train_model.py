86d18d55a9c008944f8830a98ad67545587b7e01
takumi230703 <ttaku230703@gmail.com>
2025-07-09 20:43:24 -0700

    Implement ML model

---
+++ Back-end/ml/train_model.py
@@ -0,0 +1,56 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_absolute_error
+
+soccer_player_path = "/Users/user/player-value-prediction/Back-end/ml/data/2022_2023_football_summer_transfers.csv"
+
+def train_model():
+    soccer_player_data = pd.read_csv(soccer_player_path)
+
+    y = soccer_player_data.market_value
+    bad_index = y[y == '-'].index
+    y_cleaned = y.drop(bad_index)
+
+    features = ["position", "age", "country_from", "league_from", "club_from", "country_to", "league_to", "club_to"]
+
+    X = soccer_player_data[features]
+    X_cleaned = X.drop(bad_index)
+
+    train_X, val_X, train_y, val_y = train_test_split(X_cleaned, y_cleaned, random_state=0) 
+
+    OH_encoder = OneHotEncoder(handle_unknown="ignore", sparse_output=False)
+    OH_X_train = pd.DataFrame(OH_encoder.fit_transform(train_X[features]))
+    OH_X_valid  = pd.DataFrame(OH_encoder.transform(val_X[features]))
+
+    OH_X_train.index = train_X.index
+    OH_X_valid .index = val_X.index
+
+    OH_X_train.columns = OH_X_train.columns.astype(str)
+    OH_X_valid.columns = OH_X_valid.columns.astype(str)
+
+    soccer_model = DecisionTreeRegressor(random_state=0)
+
+    soccer_model.fit(OH_X_train, train_y)
+
+    val_predictions = soccer_model.predict(OH_X_valid)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print(val_mae)
+
+    soccer_model = DecisionTreeRegressor(max_leaf_nodes=500, random_state=1)
+    soccer_model.fit(OH_X_train, train_y)
+    val_predictions = soccer_model.predict(OH_X_valid)
+    val_mae = mean_absolute_error(val_predictions, val_y)
+    print(val_mae)
+    
+
+if __name__ == "__main__":
+    train_model()
+
+
+
+
+
+
+
