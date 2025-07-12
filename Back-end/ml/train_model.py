314aa48b47120a8c1c79cef74e60092aa01172c3
takumi230703 <ttaku230703@gmail.com>
2025-07-12 16:57:44 -0700

    Implement profile.html and fix index.html

--- Back-end/ml/train_model.py
+++ Back-end/ml/train_model.py
@@ -2,7 +2,6 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
 from sklearn.preprocessing import OneHotEncoder
-from joblib import Parallel, delayed
 import joblib
 
 
