3bec1e206e9b82bc4af62d34849d01fdd3d4934d
takumi230703 <ttaku230703@gmail.com>
2025-07-23 21:08:44 -0700

    Retrieve

--- Back-end/app.py
+++ Back-end/app.py
@@ -207,6 +207,3 @@ def card():
 
     return render_template("card.html", first_name=first_name, last_name=last_name, picture_url=picture_url, prediction=prediction)
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
