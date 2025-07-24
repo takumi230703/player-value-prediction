3ffd26312eb8d5170a2ef5e6242b12cc2395dd62
takumi230703 <ttaku230703@gmail.com>
2025-07-23 20:05:16 -0700

    fix github

--- Back-end/app.py
+++ Back-end/app.py
@@ -19,7 +19,6 @@ app = Flask(__name__, template_folder=TEMPLATE_DIR, static_folder=STATIC_PATH)
 def index():
     return render_template("index.html")
 
-
 @app.route("/profile")
 def profile():
     return render_template("profile.html")
